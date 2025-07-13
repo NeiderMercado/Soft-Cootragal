1785cf84f3f84dc820bd568346695897acd1d262
NeiderMercado <neidergonzalezmercado@gmail.com>
2025-07-13 16:33:53 -0500

    Cargue de cambio denjulio

---
+++ resources/views/3-gestion.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>SOFT-COOTRAGAL</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="css/style-barra.css">
+        <script src="resources\js\js-barra-navegacion-lateral.js"></script>
+
+        <!-- Liobreria de Boostrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+        <!-- Fuente de BOOSTRAP 5 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
+
+
+        <!-- Links de estilos -->
+        <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/estilos-2.css') }}">
+
+
+    </head>
+    <body class="antialiased">              
+
+        
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+        <div class="container-fluid">
+            <a class="nav-link" href="javascript:void(0)"><img id="logo" src="/img\logo-coogal-2.2.png" alt=""></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar" id="toggler-responsive">
+            <span class="navbar-toggler-icon"></span>
+            Menú
+            </button>
+            <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav me-auto">
+                <li>
+                
+                </li> 
+                <li>
+                <a class="nav-link" href="javascript:void(0)"></a>
+                </li>
+                <li>
+                <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Inicio</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Servicios</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">¿Quienes somos?</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Contacto</a>
+                </li>
+           
+            <form class="d-flex">
+            @if (Route::has('login'))
+
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="btn btn-secondary font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Panel de inicio</a>
+                @else
+                
+                <li>
+                    <a id="btn-login" href="{{ route('login') }}" class="btn btn-primary font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Ingresar</a>
+                </li>
+                    @if (Route::has('register'))
+                <!--  
+                <a id="btn-registro" href="{{ route('register') }}" class="btn btn-success ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Registro</a>-->
+                    @endif
+                @endauth
+                </ul>
+                @endif
+            </form>
+            </div>
+        </div>
+        </nav>
+        <div class="container-fluid" id="inversion-1">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <h1 class="text-center">Inversión social</h1>
+                        <div class="container">
+                        <p>En nuestra cooperativa, el crecimiento va de la mano con la solidaridad. 
+                            Nos enorgullece contar con un alto índice de entrega por inversión social, 
+                            destinando recursos a programas que generan impacto real en la educación.<br>
+                            Creemos que el éxito se mide no solo en cifras, sino en el progreso de nuestra comunidad. Por eso, 
+                            seguimos impulsando iniciativas que mejoran la calidad de vida y fortalecen el tejido social. ¡Juntos construimos un futuro más próspero!
+                        
+                        </p>
+                        <p>
+                            _______________________________ <br>
+                            Amin Nazzer, Gerente Cootragal.
+                        </p>
+                    
+                        </div>
+
+
+                </div>
+            </div>
+        </div>
+
+        <!-- Carousel -->
+            <div id="carrusel-container" class="carousel slide" data-bs-ride="carousel">
+
+            <!-- Indicators/dots -->
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+            </div>
+
+            <!-- The slideshow/carousel -->
+            <div class="carousel-inner">
+                <div class="carousel-fondo active">
+                <img src="/img/banner/3.png" alt="Inversión social 3" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="/img/banner/6.png" alt="Inversión social 4" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="/img/banner/7.png" alt="Inversión social 5" class="d-block w-100">
+                </div>
+            </div>
+
+            <!-- Left and right controls/icons -->
+            <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </button>
+        </div>
+
+
+       
+    </body>
+</html>
